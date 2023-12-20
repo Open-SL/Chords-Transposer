ddf96f3297e66d8e0a2b4eb13f872981da1d3eb1
Nadun Indunil <nadun1indunil@gmail.com>
2023-12-20 16:51:26 +1100

    Updated minor changes.

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ const terser = require("@rollup/plugin-terser");
 module.exports = {
   input: "dist/index.js",
   output: {
-    file: "dist/bundle.min.js", // Output bundle file
+    file: "dist/chords-transposer.min.js", // Output bundle file
     name: "ChordsTransposer",
     format: "umd",
     sourcemap: true,
